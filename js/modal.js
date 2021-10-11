c09defdcf658a2e98c75db565162667fd1b135bc
Danil Prussov <danilprussov@gmai.com>
2021-10-11 13:33:09 +0600

    fix(form): valid form and swiper

--- js/modal.js
+++ js/modal.js
@@ -11,57 +11,53 @@ const  validateFields=(form,fieldsArray)=>{
   return errorFields.length === 0;
 }
 
-$('.form').submit((e)=>{
-  e.preventDefault(); 
+$('.form').submit(e => {
+  e.preventDefault();
 
-  const form =$(e.currentTarget);
+  const form = $(e.currentTarget);
   const name = form.find("[name='name']");
   const phone = form.find("[name='phone']");
   const comment = form.find("[name='comment']");
   const to = form.find("[name='to']");
 
-  const modal = $("#modal");
+  const modal = $('#modal');
   const content = modal.find(".modal__content");
+
   modal.removeClass("error-modal");
+
   const isValid = validateFields(form, [name, phone, comment, to]);
 
-  if( isValid){
-     $.ajax({
-  url: "https://webdev-api.loftschool.com/sendmail",
-  method: "post",
-  data:{
-    name:name.val(),
-    phone: phone.val(),
-    comment:comment.val(),
-    to: to.val(),
-  },
-  success: data=>{
-    content.text(data.massage)
-    
 
+  if(isValid) {
+      const request = $.ajax({
+          url: "https://webdev-api.loftschool.com/sendmail",
+          method: "post",
+          data: {
+              name: name.val(),
+              phone: phone.val(),
+              comment: comment.val(),
+              to: to.val(),
+          },
+      });
 
-    Fancybox.show([{
-        src: "#modal",
-        type: "inline"
-      }])
+      request.done(data => {
+          content.text(data.message);
+      });
 
-  },
+      request.fail(data => {
+          const message = data.responseJSON.message;
+              content.text(message);
+              modal.addClass("error-modal");
+      });
 
-  error:data=>{
-    const message = data.responseJSON.message;
-    content.text(message);
-    modal.addClass("error-modal");
-
-    Fancybox.show([{
-      src: "#modal",
-      type: "inline"
-    }])
+      request.always(() => {
+        Fancybox.show([{
+              src: "#modal",
+              type: "inline"
+          }]);
+      });
   }
-})}
-
-
-  
-})
+});
 
 $(".app--submit--btn").click((e)=>{
   e.preventDefault(); 
